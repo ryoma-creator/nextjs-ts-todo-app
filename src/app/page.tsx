468f5d239e05cffed7e4e3616f4470f8afe53672
Ryoma/Cross <ryoma111326@gmail.com>
2025-07-09 14:53:36 +0800

    ts practice...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,54 @@
-import TodoApp from '@/components/TodoApp'
+'use client'
+import { useState } from "react";
 
-export default function Home () {
-  return <TodoApp/>
-}
 
+// ==============================================
+// 問題3: TodoリストAdd機能
+// ==============================================
+// 入力フィールドとボタンで、Todoを追加できる機能を作成してください。
+// 入力後、リストに追加され、入力フィールドがクリアされる
+
+const TodoAdd = () => {
+  // ここにコードを書いてください
+  const [userInput, setUserInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleAdd = () => {  
+    setTodos([...todos, {
+      id: Date.now(),
+      input: userInput, 
+    }]);
+    setUserInput("");
+  }
+
+  return(
+    <div>
+      <div>
+        <input
+         type="text"
+         value={userInput}
+         onChange={(e)=>setUserInput(e.target.value)} 
+         placeholder="please write to do here"
+        />
+      </div>
+      <div>
+        <button
+          onClick={handleAdd}
+          className="bg-green-400 px-4 py-2"
+        >
+          add
+        </button>
+      </div>
+      <ul>
+        your to do 
+        {todos.map((todo)=>(
+          <li key={todo.id}>
+            {todo.input}
+          </li>
+))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoAdd;
--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -7,9 +7,16 @@ import { useState } from "react"
 const TodoApp = () => {
     const [todos, setTodos] = useState<string[]>([])
     const [input, setInput] = useState<string>('')
+    
     return (
         <div>
             <h1>My Todo App</h1>
+            <input
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                placeholder="new task..."
+            />
+            <button>追加</button>
             <p>Todo Count {todos.length}</p>
             <p>User Input {input}</p>
         </div>
