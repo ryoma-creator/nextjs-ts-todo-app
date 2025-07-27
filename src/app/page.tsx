38a70e0cacc8f28406ef3bbc01094de43341ad8d
Ryoma/Cross <ryoma111326@gmail.com>
2025-07-27 11:46:34 +0800

    add: display addTodo on JSX

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,23 +21,32 @@ const Todo = () => {
 
   return (
     <div style={{padding: '20px', maxWidth: '1000px', margin: '0 auto'}}>
-      <h1>TO DO LIST</h1>
-      <input
-       style={{width: '800px'}}
-       type="text" 
-       value={userInput}
-       onChange={(e)=>(setUserInput(e.target.value))}
-       onMouseEnter={(e)=>{
-        if(e.key === 'enter'){
-
-        }
-       }}
-      />
-      <button
-        onClick={addTodo}
-      >
-        add
-      </button>
+      <div><h1>TO DO LIST</h1></div>
+      <div>
+        <input
+         style={{width: '800px'}}
+         type="text"
+         value={userInput}
+         onChange={(e)=>(setUserInput(e.target.value))}
+         onKeyDown={(e)=>{
+          if(e.key === 'Enter'){
+            addTodo
+          }
+         }}
+        />
+        <button
+          onClick={addTodo}
+        >
+          add
+        </button>
+      </div>
+      <ul>
+         {todos.map(todo=>(
+          <li key={todo.id}>
+            {todo.text}
+          </li>
+         ))}
+      </ul>
     </div>
   )
 }
