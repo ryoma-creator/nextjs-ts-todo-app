2a7470b3e123ccd8524ffc1dc7022f8ff57ebaa7
Ryoma/Cross <ryoma111326@gmail.com>
2025-07-27 11:34:36 +0800

    add: addTodo function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,20 @@ import React from 'react'
 import { useState } from 'react'
 
 const Todo = () => {
+  const [userInput, setUserInput] = useState('');
+  const [todos, setTodos] = useState([])
 
+  const addTodo = () => {
+    if(!userInput.trim()) return;
+    const newTodo = {
+      id: crypto.randomUUID(),
+      text: userInput,
+    }
+    setTodos([
+      ...todos, newTodo
+    ]);
+    setUserInput('');
+  }
 
   return (
     <div style={{padding: '20px', maxWidth: '1000px', margin: '0 auto'}}>
@@ -12,8 +25,19 @@ const Todo = () => {
       <input
        style={{width: '800px'}}
        type="text" 
+       value={userInput}
+       onChange={(e)=>(setUserInput(e.target.value))}
+       onMouseEnter={(e)=>{
+        if(e.key === 'enter'){
 
+        }
+       }}
       />
+      <button
+        onClick={addTodo}
+      >
+        add
+      </button>
     </div>
   )
 }
