eb0e68badf09600a72360afa6a67872898363631
Ryoma/Cross <ryoma111326@gmail.com>
2025-08-09 11:54:40 +0800

    practice form validatoin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,20 @@ const FormValidation = () => {
   const [errors, setErrors] = useState({})
   const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const handleSubmit = () => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // const name = e.target.name
+    // const value = e.target.value
+    
+    setFormData(prev => ({...prev, [name]: value}));
+
+    // 方法2: 直接書く（長い）
+    // setFormData(prev => ({...prev, [e.target.name]: e.target.value}));
+
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
     const newErrors = {};
     
     if(!formData.name.trim()){
@@ -25,19 +38,68 @@ const FormValidation = () => {
       newErrors.email = 'メールが無効です';
     }
 
-    if(age < 0 || age > 100 || NaN(formData.age)){
+    const ageNum = Number(formData.age);
+    if (isNaN(ageNum) || ageNum < 0 || ageNum > 100) {
       newErrors.age = '年齢が無効です';
     }
-
+    console.log('newErrors:', newErrors); // ← 追加！
     setErrors(newErrors);
 
     if (Object.keys(newErrors).length === 0) {
-      alert('成功');
+      // Object.keys(obj) は、オブジェクトの「キー（プロパティ名）」を配列として返す関数。
+      setIsSubmitted(true);
+      setTimeout(()=> setIsSubmitted(false),3000);
     } 
   };
 
   return (
-    <div>FormValidation</div>
+    <div>
+      {isSubmitted ? (<div style={{color: 'green'}}>Success to submit!</div>
+      ) : (
+      <>
+      <h1>FormValidation</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="enter your name"
+            />
+            {errors.name && <p>{errors.name}</p>}
+          </div>
+          <div>
+            <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="enter your email"
+            />
+            {errors.email && <p>{errors.email}</p>}
+          </div>
+          <div>
+            <input
+            type="number"
+            name="age"
+            value={formData.age}
+            onChange={handleChange}
+            placeholder="enter your age"
+            />
+            {errors.age && <p>{errors.age}</p>}
+          </div>
+          <div>
+            <button
+             type="submit"
+            >
+              SUBMIT
+            </button>
+          </div>
+        </form>
+        </>
+)}
+    </div>
   )
 }
 
