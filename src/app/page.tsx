22766fbd3645294b83c384c5bc887cd1279e7b21
Ryoma/Cross <ryoma111326@gmail.com>
2025-08-13 19:47:14 +0800

    practice data fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,106 +1,49 @@
 'use client'
 
-import { useState } from "react"
-
+import { useState, useEffect } from "react"
 import React from 'react'
 
-const FormValidation = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    age: "",
-  });
 
-  const [errors, setErrors] = useState({})
-  const [isSubmitted, setIsSubmitted] = useState(false);
+const ApiDataApp = () => {
+  const [fetchData, setFetchData] = useState<any[]>([]);
+  const[loading, setLoading] = useState<boolean>(false);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    // const name = e.target.name
-    // const value = e.target.value
-    
-    setFormData(prev => ({...prev, [name]: value}));
 
-    // 方法2: 直接書く（長い）
-    // setFormData(prev => ({...prev, [e.target.name]: e.target.value}));
+  useEffect(()=>{
+    const controller = new AbortController();
+    const dataFetch=async():Promise<void>=>{
+      setLoading(true);
+      try{
+        const response = await fetch(('https://jsonplaceholder.typicode.com/users'),{
+          signal: controller.signal
+        });
+        if(!response.ok){
+          throw new Error(`HTTP Error Number ${response.status}`); 
+        }
+        const data = await response.json();
+        console.log(data);
+        setFetchData(data); 
+      }
+      catch(){
 
+      }
+      finally{
+
+      }
+    };
+
+    return ()=> controller.abort();
+  },[]);
+
+  if(loading){
+    return (<div>Loading...</div>)
   }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const newErrors = {};
-    
-    if(!formData.name.trim()){
-      newErrors.name = '名前が未入力です';
-    }
-
-    if(!formData.email.includes('@')){
-      newErrors.email = 'メールが無効です';
-    }
-
-    const ageNum = Number(formData.age);
-    if (isNaN(ageNum) || ageNum < 0 || ageNum > 100) {
-      newErrors.age = '年齢が無効です';
-    }
-    console.log('newErrors:', newErrors); // ← 追加！
-    setErrors(newErrors);
-
-    if (Object.keys(newErrors).length === 0) {
-      // Object.keys(obj) は、オブジェクトの「キー（プロパティ名）」を配列として返す関数。
-      setIsSubmitted(true);
-      setTimeout(()=> setIsSubmitted(false),3000);
-    } 
-  };
 
   return (
     <div>
-      {isSubmitted ? (<div style={{color: 'green'}}>Success to submit!</div>
-      ) : (
-      <>
-      <h1>FormValidation</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            placeholder="enter your name"
-            />
-            {errors.name && <p>{errors.name}</p>}
-          </div>
-          <div>
-            <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            placeholder="enter your email"
-            />
-            {errors.email && <p>{errors.email}</p>}
-          </div>
-          <div>
-            <input
-            type="number"
-            name="age"
-            value={formData.age}
-            onChange={handleChange}
-            placeholder="enter your age"
-            />
-            {errors.age && <p>{errors.age}</p>}
-          </div>
-          <div>
-            <button
-             type="submit"
-            >
-              SUBMIT
-            </button>
-          </div>
-        </form>
-        </>
-)}
+
     </div>
   )
 }
 
-export default FormValidation
+export default ApiDataApp
